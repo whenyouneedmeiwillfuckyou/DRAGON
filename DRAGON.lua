422a5b5b80e2e25a98d4832acce420a661183ae1
whenyouneedmeiwillfuckyou <84554252+whenyouneedmeiwillfuckyou@users.noreply.github.com>
2021-05-25 00:00:01 +0200

    Update DRAGON.lua

--- DRAGON.lua
+++ DRAGON.lua
@@ -2755,6 +2755,26 @@ keyboard = {}
 keyboard.inline_keyboard = {
 {{text = '  ❨ M A N I A L ♕  ❩ ',url="t.me/I_B_N_M_A_N_I_A_L"}},
 }
+if text == 'ديشا' or text == 'يا ديشا' or text == 'واد ياديشا' or text == 'ياديشا' or text == 'ديشا فين' then
+local Text = [[
+المطور ديشا لو حابب تتواصل معاه
+اتبع الزر إلى تحت ↓
+
+]]
+keyboard = {} 
+keyboard.inline_keyboard = {
+{{text = '  ❨ D E S H A ♕  ❩ ',url="t.me/DESHAXBLACK"}},
+}
+if text == 'مسي' or text == 'يا ميسي' or text == 'ميسي' or text == 'يا مسي' or text == 'ميسي فين' then
+local Text = [[
+المطور مسي لو حابب تتواصل معاه
+اتبع الزر إلى تحت ↓
+
+]]
+keyboard = {} 
+keyboard.inline_keyboard = {
+{{text = '  ❨ D E S H A ♕  ❩ ',url="t.me/MESSIX_BLACK"}},
+}
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
