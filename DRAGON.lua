a513e7b69f910b0d184373022bfc94a590b58419
whenyouneedmeiwillfuckyou <84554252+whenyouneedmeiwillfuckyou@users.noreply.github.com>
2021-05-24 23:21:15 +0200

    Update DRAGON.lua

--- DRAGON.lua
+++ DRAGON.lua
@@ -2716,7 +2716,7 @@ keyboard.inline_keyboard = {
 {{text = '  ❨ SＯＵＲＣＥ ♕  ❩ ', url="t.me/CH_POWLER"}},
 }
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+return https.request("https://api.telegram.org/bot"..token..'/editMessageText?chat_id='..Chat_id..'&text='..URL.escape(Teext)..'&message_id='..msg_idd..'&parse_mode=markdown&disable_web_page_preview=true&reply_markup='..JSON.encode(keyboard)) 
 end
 
 if text == 'مين نصبلك' or text == 'عايزه بوت' or text == 'عايز بوت' then
