2f48a52b8fa9bc86626d9cc9ef0abed271a90c7b
whenyouneedmeiwillfuckyou <84554252+whenyouneedmeiwillfuckyou@users.noreply.github.com>
2021-05-25 00:25:39 +0200

    Update DRAGON.lua

--- DRAGON.lua
+++ DRAGON.lua
@@ -2704,14 +2704,15 @@ end,nil)
 end
 if text == 'السورس' or text == 'سورس' or text == 'يا سورس' or text == 'source' then
 local Text = [[
- 🕊  مرحبا بك في سورس مسلم بلص  🕊 
+ 🕊  مرحبا بك في سورس باولر الاعظم  🕊 
 اتبع الازرار إلى تحت ↓
 لدخول لحسابات السورس 
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
-{{text = '  ❨ L E G A N D 🕊  ❩ ',url="t.me/L_E_p"}},
-{{text = '  ❨ SＯＵＲＣＥ 🕊  ❩ ', url="t.me/UU_Le2"}},
+{{text = '  ❨ M A N I A L ♕  ❩ ',url="t.me/I_B_N_M_A_N_I_A_L"}},
+
+{{text = '  ❨ SＯＵＲＣＥ ♕  ❩ ', url="t.me/CH_POWLER"}},
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
