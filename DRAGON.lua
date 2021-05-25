f458af5fb821426d2d4c9c08afb137633c158738
whenyouneedmeiwillfuckyou <84554252+whenyouneedmeiwillfuckyou@users.noreply.github.com>
2021-05-25 03:37:57 +0200

    Update DRAGON.lua

--- DRAGON.lua
+++ DRAGON.lua
@@ -2712,7 +2712,6 @@ local Text = [[
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '  ❨ M A N I A L ♕  ❩ ',url="t.me/I_B_N_M_A_N_I_A_L"},{text = '  ❨ L E G A N D ♕  ❩ ',url="t.me/L_E_p"}},
-{{text = '  ❨ D E S H A ♕  ❩ ',url="t.me/DESHAXBLACK"},{text = '  ❨ M E S S I ♕  ❩ ',url="t.me/MESSIX_BLACK"}},
 {{text = '  ❨ SＯＵＲＣＥ ♕  ❩ ', url="t.me/CH_POWLER"}},
 }
 local msg_id = msg.id_/2097152/0.5
