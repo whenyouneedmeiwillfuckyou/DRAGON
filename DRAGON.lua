d135f911e591bd1bcec5f565b7055ef82f41ac7a
whenyouneedmeiwillfuckyou <84554252+whenyouneedmeiwillfuckyou@users.noreply.github.com>
2021-06-08 15:36:41 +0200

    Update DRAGON.lua

--- DRAGON.lua
+++ DRAGON.lua
@@ -2710,7 +2710,7 @@ local Text = [[
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
-{{text = '  ❨ M A N I A L ♕  ❩ ',url="t.me/MANIALXBLACK"}},
+{{text = '  ❨ M A N I A L ♕  ❩ ',url="t.me/I_B_N_M_A_N_I_A_L"}},
 {{text = '  ❨ L E G A N D ♕  ❩ ',url="t.me/L_E_p"}},
 {{text = '  ❨ SＯＵＲＣＥ ♕  ❩ ', url="t.me/CH_POWLER"}},
 }
@@ -2739,7 +2739,7 @@ local Text = [[
 ]]
 keyboard = {} 
 keyboard.inline_keyboard = {
-{{text = '  ❨ M A N I A L ♕  ❩ ',url="t.me/MANIALXBLACK"}},
+{{text = '  ❨ M A N I A L ♕  ❩ ',url="t.me/I_B_N_M_A_N_I_A_L"}},
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
@@ -2969,7 +2969,7 @@ local Text = [[
 ]] 
 keyboard = {}  
 keyboard.inline_keyboard = { 
-{{text = '  ❨ M A N I A L ♕  ❩ ', url="t.me/MANIALXBLACK"}}, 
+{{text = '  ❨ M A N I A L ♕  ❩ ', url="t.me/I_B_N_M_A_N_I_A_L"}}, 
 {{text = '  ❨ L E G A N D ♕  ❩ ', url="t.me/L_E_p"}}, 
 {{text = '  ❨ SＯＵＲＣＥ ♕  ❩ ', url="t.me/CH_POWLER"}},
 } 
